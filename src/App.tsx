fb42dbd948d0c06e217e9425e6fa69340a24207e
lukasrvs <lucas_antonio.v@hotmail.com>
2021-11-25 23:35:45 -0300

    adding files to repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useEffect } from 'react';
+import * as Component from './App.styles';
+import { Character } from './components/Character';
+import { useCharacter } from './hooks/useCharacter';
+const App = () =>{
+  const char = useCharacter('Bonitão');
+  const char2 = useCharacter('Zoado');
+  useEffect(()=>{
+    window.addEventListener('keydown', handleKeyDown);
+  },[]);
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch(e.code) {
+      case 'KeyA':
+      case 'ArrowLeft': 
+        char.moveLeft();
+        break;
+      case 'KeyW':
+      case 'ArrowUp':
+        char.moveUp();
+        break;
+      case 'KeyD':
+      case 'ArrowRight':
+        char.moveRight();
+        break;
+      case 'KeyS':
+      case 'ArrowDown':
+        char.moveDown();
+        break;
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Component.Container>
+      <Component.Map>
+        <Character x={char.x} y={char.y} side={char.side} name={char.name} />
+        <Character x={char2.x} y={char2.y} side={char2.side} name={char2.name} />
+      </Component.Map>
+    </Component.Container>
   );
 }
-
 export default App;
